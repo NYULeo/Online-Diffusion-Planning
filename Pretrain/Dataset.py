6aad5580f6741eede0b7af4d7fcda9aa341b795a
NYULeo <leo@nyu.edu>
2025-09-02 01:09:41 -0700

    Update Dataset.py and pretrain_planner.py

--- Pretrain/Dataset.py
+++ Pretrain/Dataset.py
@@ -11,7 +11,7 @@ def get_env(env_name, specific_env):
     #env= data.get_env()
     d_s = data.get_state_dim()
     d_a = data.get_action_dim()
-    return   d_s, d_a
+    return  d_s, d_a
 
 
 def get_dataset(name: str, specific_name: str):
