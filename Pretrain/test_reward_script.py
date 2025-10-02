61ee3dec2a0a1971e5aff43319920c8e154795e2
NYULeo <leo@nyu.edu>
2025-10-01 21:42:11 -0700

    Add 2DMaze reward models, update test script and add demo video

--- Pretrain/test_reward_script.py
+++ Pretrain/test_reward_script.py
@@ -5,8 +5,8 @@ from utils import set_seed
 if __name__ == '__main__':
     set_seed(1)
     test_reward(
-    dataset_name = 'kitchen', 
-    specific_dataset = None, 
+    dataset_name = 'pointmaze', 
+    specific_dataset = 'medium', 
     sigma = 3, 
-    save_freq = 50, 
-    num_steps = 1000)
+    save_freq = 100, 
+    num_steps = 10000)
