d172a289b5c976250c76867b30b1f1416f81763e
NYULeo <leo@nyu.edu>
2025-10-06 18:35:30 -0700

    Update test_kernel_script.py

--- Pretrain/test_kernel_script.py
+++ Pretrain/test_kernel_script.py
@@ -7,9 +7,10 @@ import pickle
 
 if __name__ == '__main__':  # pragma: no cover
     set_seed(1)
-    
+    """
     with open('Rollouts/kitchen/partial/Generated_trajs_Info.pkl', 'rb') as f:
          trajs_info = pickle.load(f)
     trajs = trajs_info['trajs']
-    test_Model(dataset_name = 'kitchen', trajs = trajs, save_freq = 5000, num_steps = 300000)
-    #test_Model(dataset_name = 'kitchen', save_freq = 5000, num_steps = 300000)
+    """
+    #test_Model(dataset_name = 'kitchen', trajs = trajs, save_freq = 5000, num_steps = 300000)
+    test_Model(dataset_name = 'kitchen', save_freq = 5000, num_steps = 300000)
