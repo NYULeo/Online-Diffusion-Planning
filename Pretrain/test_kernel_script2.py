386380b057fe7f364c4573910becb573ec7a7391
NYULeo <leo@nyu.edu>
2025-10-07 22:19:27 -0700

    Add test_kernel_script2.py

---
+++ Pretrain/test_kernel_script2.py
@@ -0,0 +1,15 @@
+from Transition_Kernel.Kernel_Backbone import test_Model
+from utils import set_seed
+import pickle
+
+
+
+
+if __name__ == '__main__':  # pragma: no cover
+    set_seed(1)
+    with open('Rollouts/kitchen/partial/Generated_trajs_Info.pkl', 'rb') as f:
+         trajs_info = pickle.load(f)
+    trajs = trajs_info['trajs']
+   
+    #test_Model(dataset_name = 'pointmaze', specific_dataset = 'medium', trajs = trajs, save_freq = 2000, num_steps = 50000)
+    test_Model(dataset_name = 'kitchen', save_freq = 2000, num_steps = 500000)
