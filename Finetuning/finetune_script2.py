b59cc86fa470bcd994a058c6acdac1243c5dce04
NYULeo <leo@nyu.edu>
2025-10-25 04:05:45 -0700

    Update finetune_script2.py

--- Finetuning/finetune_script2.py
+++ Finetuning/finetune_script2.py
@@ -7,9 +7,19 @@ from Finetune_Backbone import OnlineFinetuner, FinetuningConfig
 from adjoint_matching import AdjointMatchingConfig
 from acc_adjoint_matching import Acc_AdjointMatchingConfig
 from traj_reward import RewardConfig
-from utils import set_seed
+import random
+import numpy as np
+import torch
 
-
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
               
 if __name__ == "__main__":
     # Example usage of the Adjoint Matching training without a dataset.
