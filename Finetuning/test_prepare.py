35137c795645b952a3dca2174b92dfe1b9f1e6f0
NYULeo <leo@nyu.edu>
2025-10-26 04:15:51 -0700

    Add test_prepare.py

---
+++ Finetuning/test_prepare.py
@@ -0,0 +1,19 @@
+from sched import scheduler
+import torch.nn as nn
+from accelerate import Accelerator
+import torch
+
+def main():
+    accelerator = Accelerator()
+    model = nn.Linear(10, 10)  # Simple model
+    optimizer = torch.optim.Adam(model.parameters())
+    data = torch.randn(10, 10)
+    dataloader = dataloader(data, batch_size = 2, shuffle = True, num_workers = 0, pin_memory = True)
+    print(f"Process {accelerator.process_index}: Starting prepare...")
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max = 100)
+    model, optimizer, dataloader, scheduler = accelerator.prepare(model, optimizer, dataloader, scheduler)
+    print(f"Process {accelerator.process_index}: Prepare done!")
+
+if __name__ == "__main__":
+    torch.manual_seed(1)
+    main()
