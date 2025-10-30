233d04489b9a85595f24209ad904474551e28508
NYULeo <leo@nyu.edu>
2025-10-30 16:47:48 -0700

    Update utils.py

--- Finetuning/utils.py
+++ Finetuning/utils.py
@@ -24,8 +24,8 @@ class Lambda:
         self.eta_lam = eta_lam
     def update(self, C):
         delta = F.softplus(torch.tensor([0.0], requires_grad = False), beta = self.beta)
-        #self.lam = 0.0
-        self.lam = np.maximum(0.0, self.lam + self.eta_lam * (C - delta.item()))
+        self.lam = 0.0
+        #self.lam = np.maximum(0.0, self.lam + self.eta_lam * (C - delta.item()))
     def get_lam(self):
         return self.lam
 
