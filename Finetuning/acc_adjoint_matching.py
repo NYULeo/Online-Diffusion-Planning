3c19d419c7df7b8aa39056352dc0fee9bb5126fa
NYULeo <leo@nyu.edu>
2025-11-05 03:09:22 -0800

    Update acc_adjoint_matching.py

--- Finetuning/acc_adjoint_matching.py
+++ Finetuning/acc_adjoint_matching.py
@@ -387,7 +387,9 @@ class Acc_AdjointMatchingFineTuner:
                 loss_tensor = self.adjoint_matching_loss(traj, adjoint)  # tensor with grad
                 local_loss_tensors.append(loss_tensor)
                 local_rewards.append(reward)
-            
+                print(f"Loss: {loss_tensor}")
+                print(f"Reward: {reward}")
+            exit()
             local_loss = torch.stack(local_loss_tensors).mean()
             local_rewards = torch.stack(local_rewards).mean()
             
