58caf1726bca455bad41565e52b1ec3525d82028
NYULeo <leo@nyu.edu>
2025-11-21 20:49:00 -0800

    Update finetune_script2.py

--- Finetuning/finetune_script2.py
+++ Finetuning/finetune_script2.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         reward_model_checkpoint = 100000,
         kernel_model_checkpoint = 34000,
         finetune_steps = 3000,
-        diffusion_steps = 200,
+        diffusion_steps = 500,
         finetune_batch_size = 12,
         finetune_lr = 2e-05,
         inital_lam = 0.01,
