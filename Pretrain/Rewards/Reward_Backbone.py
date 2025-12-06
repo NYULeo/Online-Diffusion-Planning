c33d03a12f02dc9815a8dd0b010a3b1fe3564054
NYULeo <leo@nyu.edu>
2025-12-06 06:25:59 -0800

    Remove unused import from Reward_Backbone

--- Pretrain/Rewards/Reward_Backbone.py
+++ Pretrain/Rewards/Reward_Backbone.py
@@ -2,8 +2,6 @@
 
 import sys
 import os
-
-from torch._dynamo.pgo import ReservedWorkflowIdUserError
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.chdir(project_root)
