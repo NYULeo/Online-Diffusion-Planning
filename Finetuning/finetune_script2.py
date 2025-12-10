ef988d9f55aea0dd76f24f5662ae76b49b148f95
NYULeo <leo@nyu.edu>
2025-12-10 03:22:50 -0800

    Update Rollout and finetune_script2

--- Finetuning/finetune_script2.py
+++ Finetuning/finetune_script2.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
         update_lambda_every = 1,
         reward_scaling_factor = 50,
         MaxEnt = True,
-        Entropy_Scaling_Factor = 1.0)
+        Entropy_Scaling_Factor = 0.5)
     set_seed(1)
     OnlineFinetuner = OnlineFinetuner(FTConfig)
     OnlineFinetuner.finetune_planner()
