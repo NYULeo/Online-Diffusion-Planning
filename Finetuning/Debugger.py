32f4c753051504b85900f3b978f02c77713d44ee
NYULeo <leo@nyu.edu>
2025-12-11 00:25:08 -0800

    Update Debugger.py

--- Finetuning/Debugger.py
+++ Finetuning/Debugger.py
@@ -37,7 +37,7 @@ except ImportError as e:
     MATPLOTLIB_AVAILABLE = False
     plt = None
 import minari
-from Pretrain.Rewards.nets import SimpleReward, Reward
+from Pretrain.Rewards.nets import SimpleReward, Reward, DeepScaledReward
 from Pretrain.Rewards.Reward_Backbone import get_pretrained_reward, get_pretrained_reward_stats
 import random
 from torch.utils.data import DistributedSampler, DataLoader
@@ -151,8 +151,9 @@ def heatmap(STEP):
    # ================== Load Reward Model ==================
    #print(f"Loading reward model (step {STEP})...")
    state_dict, obs_dim, act_dim, name = get_pretrained_reward('pointmaze', STEP, 'medium')
-   model = SimpleReward(obs_dim, act_dim)
+   #model = SimpleReward(obs_dim, act_dim)
    #model = Reward(obs_dim, act_dim)
+   model = DeepScaledReward(obs_dim, act_dim)
    model.load_state_dict(state_dict)
    model.eval()
    stats = get_pretrained_reward_stats(name)
