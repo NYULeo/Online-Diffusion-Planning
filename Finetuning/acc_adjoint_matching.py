d537fd5d6f9d14544d6de379ed8cdef032d4acf0
NYULeo <leo@nyu.edu>
2025-12-13 14:45:36 -0800

    Update Rollout and acc_adjoint_matching

--- Finetuning/acc_adjoint_matching.py
+++ Finetuning/acc_adjoint_matching.py
@@ -395,9 +395,9 @@ class Acc_AdjointMatchingFineTuner:
         t_asc_reversed = torch.flip(self.t_asc, dims = [0]).to(self.device)
         k_reversed = torch.flip(self.k, dims = [0]).to(self.device)
        
-        a0 = ( (self.config.reward_scaling_factor/reward_std) * gradient).detach().unsqueeze(0).to(self.device) + (self.config.Entropy_Scaling_Factor * EntGrad)
-        max_norm = 5.0
-        a0 =   a0 * torch.clamp(max_norm / torch.norm(a0), max=1.0)
+        a0 =  ( 0.0 * (self.config.reward_scaling_factor/reward_std) * gradient).detach().unsqueeze(0).to(self.device) + (self.config.Entropy_Scaling_Factor * EntGrad)
+        #max_norm = 5.0
+        #a0 =   a0 * torch.clamp(max_norm / torch.norm(a0), max=1.0)
         print(f"a0: {a0.norm().item()}")
         
         a.append(a0)
