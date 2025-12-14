352c01e2d0447d2476cee450cf5e08f8ea73a82a
NYULeo <leo@nyu.edu>
2025-12-13 21:39:11 -0800

    Update Debugger and train_reward_script

--- Pretrain/train_reward_script.py
+++ Pretrain/train_reward_script.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     train_reward(
     dataset_name = 'pointmaze',
     batch_size = 256, 
-    num_steps = 1000, 
-    save_freq = 200,  
+    num_steps = 10000, 
+    save_freq = 2000,  
     lr = 3e-4,
     sigma = 100.0,
     target_reward = 50.0,
