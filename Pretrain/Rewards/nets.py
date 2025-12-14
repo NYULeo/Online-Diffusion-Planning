29829534c691dc0613e2764374effb3f6d6f0fe6
NYULeo <leo@nyu.edu>
2025-12-13 20:23:43 -0800

    Update nets.py

--- Pretrain/Rewards/nets.py
+++ Pretrain/Rewards/nets.py
@@ -489,7 +489,7 @@ class EnsembleModel(nn.Module):
 
 
 class SimpleReward(nn.Module):
-    def __init__(self, obs_dim, act_dim, hidden=64):
+    def __init__(self, obs_dim, act_dim, hidden=32):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(obs_dim + act_dim, hidden), 
