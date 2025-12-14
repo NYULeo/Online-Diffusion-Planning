470b970e7f8e7e467f82b3a3f5ae3bb821e0ae3b
NYULeo <leo@nyu.edu>
2025-12-13 19:01:49 -0800

    Update Reward_Backbone.py

--- Pretrain/Rewards/Reward_Backbone.py
+++ Pretrain/Rewards/Reward_Backbone.py
@@ -18,7 +18,7 @@ from Pretrain.utils import set_seed, SAStats
 #from Critic.train_critic import get_CriticName
 import torch.nn as nn
 import pickle
-from Pretrain.Rewards.nets import Reward, MLPNetwork, ScalarReward, SimpleReward, DeepScaledReward
+from Pretrain.Rewards.nets import Reward, MLPNetwork, ScalarReward, SimpleReward
 import os
 from scipy.ndimage import gaussian_filter1d, convolve
 from Pretrain.utils import cycle
