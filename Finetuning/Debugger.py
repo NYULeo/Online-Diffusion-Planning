9f0cb40e7dcac0122aecb2e675ffec79504aac5f
NYULeo <leo@nyu.edu>
2025-12-16 19:57:07 -0800

    Update Debugger.py

--- Finetuning/Debugger.py
+++ Finetuning/Debugger.py
@@ -663,14 +663,14 @@ def heatmap(STEP, agg_method='max', highlight_negatives=True):
 
 if __name__ == '__main__':
    
-   step = 200
-   while(step <= 1000):
+   step = 500
+   while(step <= 5000):
        np.random.seed(0)
        random.seed(0)
        torch.manual_seed(0) 
        #print(f"Ploting the heatmap for checkpoint {step}")
        heatmap(step)
-       step += 200
+       step += 500
    print('Done')
 
 
