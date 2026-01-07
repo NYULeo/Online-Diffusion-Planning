28cc85d4160951f100c052d15aad0e2c20b7c075
NYULeo <leo@nyu.edu>
2026-01-06 21:59:46 -0800

    Update Dataset.py: remove test/debug code

--- Pretrain/Dataset.py
+++ Pretrain/Dataset.py
@@ -694,8 +694,3 @@ def visualize_clusters(vectors, assignments, cluster_centers, title_prefix=""):
 # Usage:
 #visualize_clusters(vectors, assignments, stats['cluster_centers'], "Kitchen Rewards: ")
 """
-
-a = [1,2,3,4]
-b = [5,6,7,8]
-a.extend(b)
-print(a)
