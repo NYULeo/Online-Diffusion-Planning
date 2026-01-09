f224eeae315065a0e58dc7c03d45c8ddc969a329
NYULeo <leo@nyu.edu>
2026-01-09 06:50:58 -0800

    Update test_kernel_script2.py: simplify get_trajs call to use positional arguments

--- Pretrain/test_kernel_script2.py
+++ Pretrain/test_kernel_script2.py
@@ -12,7 +12,7 @@ import pickle
 
 if __name__ == '__main__':  # pragma: no cover
     set_seed(1)
-    trajs = get_trajs(dataset_name = 'kitchen', specific_env = 'partial', step = 0)
+    trajs = get_trajs('kitchen', 'partial', step = 0)
    
     #test_Model(dataset_name = 'pointmaze', specific_dataset = 'medium', trajs = trajs, save_freq = 2000, num_steps = 50000)
     test_kernel(dataset_name = 'kitchen', trajs = trajs, save_freq = 5000, num_steps = 50000, ensemble_size = 10)
