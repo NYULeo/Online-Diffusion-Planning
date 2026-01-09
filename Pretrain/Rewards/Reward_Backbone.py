6e2260daf2c17266c654b9977bed7b8f9e79aee2
NYULeo <leo@nyu.edu>
2026-01-09 00:06:27 -0800

    Update Reward_Backbone.py: remove unused import

--- Pretrain/Rewards/Reward_Backbone.py
+++ Pretrain/Rewards/Reward_Backbone.py
@@ -1,7 +1,6 @@
 import sys
 import os
 from sympy.printing.rcode import reserved_words
-from torch.distributions.utils import R
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.chdir(project_root)
