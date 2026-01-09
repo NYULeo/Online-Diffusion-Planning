Update Python files: add expert score normalization, fix dataset-specific handling in reward and kernel training

Finetuning/utils.py
@@ -31,20 +31,23 @@ from gymnasium.vector import AsyncVectorEnv
 from Pretrain.Planners.Backbone.Sampler import sample_euler_karras
 from Pretrain.Planners.Backbone.Dit import DiT1d
 from Pretrain.Critic.nets import Critic
+from Pretrain.Dataset import get_dataset
 import json
 
+
+
 def spare_reward_prcocessor(rewards):
-         Temp = []
-         for i in range(1, len(rewards)):
-            if(rewards[i] == rewards[i-1]+1):
-                  Temp.append(i)
-         new_rewards = [0]*len(rewards)
-         for i in range(len(rewards)):
-             if(i in Temp):
-                  new_rewards[i] = 1
-             else:
-                  new_rewards[i] = 0
-         return np.array(new_rewards, dtype = np.float64) 
+    Temp = []
+    for i in range(1, len(rewards)):
+        if(rewards[i] == rewards[i-1]+1):
+            Temp.append(i)
+    new_rewards = [0]*len(rewards)
+    for i in range(len(rewards)):
+        if(i in Temp):
+            new_rewards[i] = 1
+        else:
+            new_rewards[i] = 0
+    return np.array(new_rewards, dtype = np.float64) 
 
 def reward_filter(obs, rews, goal):
     #target_goals = np.array([[-2.5, -2.5], [2.5, 2.5], [2.5, -2.5], [-2.5, 2.5]])
@@ -730,7 +733,7 @@ def clip_actions(x: torch.Tensor, d_s: int) -> torch.Tensor:
     x[..., d_s:] = actions
     return x
 
-def get_normalized_score(trajs):
+def get_normalized_score(trajs, expert_score: Optional[float] = None):
     total = 0.0
     for i in range(len(trajs)):
         temp = 0.0
@@ -741,8 +744,17 @@ def get_normalized_score(trajs):
     avg_discounted_return = total / len(trajs)
     # 5. Compute normalized score
     normalized_score = 100 * avg_discounted_return 
+    if(expert_score is not None):
+        normalized_score = 100 * (normalized_score / expert_score)
     #print(f"Normalized Score: {normalized_score:.2f}")
     return normalized_score
+
+def get_expert_score(dataset_name):
+    if(dataset_name == 'kitchen'):
+         data = get_dataset(dataset_name, 'complete')
+    trajs = data.get_trajectories()
+    score = get_normalized_score(trajs, None)
+    return score
 
 def rollout_parallel(env_name, specific_env, horizon = 32, steps_T = 50, num_karras = 10, eta = 0.8, episode_length = 4000, checkpoint_step = 1000000, num_envs=8, goal_cell: Optional[np.ndarray] = None, device: torch.device = None, seed_base: int = 0):
      #print(f"Horizon: {horizon}, step_T: {steps_T}, eta: {eta}, critic: {critic}, Checkpoint_steps: {checkpoint_steps}")
@@ -886,7 +898,9 @@ def rollout_parallel(env_name, specific_env, horizon = 32, steps_T = 50, num_kar
           })
         
      vec_env.close()
-     score = get_normalized_score(trajs)
+     if(goal_cell is None):
+          expert_score = get_expert_score(env_name)
+     score = get_normalized_score(trajs, expert_score)
      save_trajs(trajs, env_name, specific_env, checkpoint_step)
      #print(f"Average Normalized Score: {score:.2f}")
      return trajs, score, total_steps
@@ -895,3 +909,5 @@ def load_hyperparameters(filepath: str) -> Dict:
     with open(filepath, 'r') as f:
         hyperparams = json.load(f)
     return hyperparams
+
+

Pretrain/Rewards/Reward_Backbone.py
@@ -24,6 +24,12 @@ import torch.nn.functional as F
 import numpy as np
 import json
 
+def check_specifc_dataset(dataset_name):
+    if(dataset_name == 'kitchen'):
+         return False
+    elif(dataset_name == 'pointmaze'):
+         return True
+
 def getName(env_name, specific_env):
      if(env_name == 'kitchen'):
           return 'Kitchen'
@@ -62,18 +68,16 @@ def getName(env_name, specific_env):
      else:
          raise ValueError(f"Invalid environment name: {env_name}")
 
+
 def save_reward_hyperparameters(dataset_name, batch_size, num_steps, lr, sigma, 
                                   obs_dim, act_dim, reward_name, optimizer, reward_net, filepath: Optional[str] = None, 
                                   specific_dataset: Optional[str] = None, target_reward: Optional[float] = None,
                                   goal: Optional[np.array] = None):
     
     if filepath is None:
-        if specific_dataset is None:
-            os.makedirs(f"./Pretrain/Rewards/{dataset_name}/args/", exist_ok=True)
-            filepath = f"./Pretrain/Rewards/{dataset_name}/args/hyperparameters.json"
-        else:
-            os.makedirs(f"./Pretrain/Rewards/{dataset_name}/{specific_dataset}/args/", exist_ok=True)
-            filepath = f"./Pretrain/Rewards/{dataset_name}/{specific_dataset}/args/hyperparameters.json"
+        os.makedirs(f"./Pretrain/Rewards/{reward_name}/args/", exist_ok=True)
+        filepath = f"./Pretrain/Rewards/{reward_name}/args/hyperparameters.json"
+
     
     def convert_to_json_serializable(obj):
         """Recursively convert objects to JSON-serializable types"""
@@ -307,7 +311,6 @@ class RewardDataset(Dataset):
             torch.tensor(r, dtype=torch.float32),
         )
     
-
 def train_reward(dataset_name: str, batch_size, num_steps, save_freq, lr, sigma, target_reward: Optional[float] = None, specific_dataset: Optional[str] = None, goal: Optional[np.array] = None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     trajs, reward_name, obs_dim, act_dim = Train_Dataset(dataset_name, specific_dataset)
@@ -316,6 +319,10 @@ def train_reward(dataset_name: str, batch_size, num_steps, save_freq, lr, sigma,
     dataloader = cycle(DataLoader(dataset, batch_size = batch_size, shuffle = True, pin_memory = True, num_workers = 8))
     reward_net = SimpleReward(obs_dim, act_dim).to(device)
     optimizer = optim.AdamW(reward_net.parameters(), lr = lr, weight_decay = 1e-4)
+    if(check_specifc_dataset(dataset_name)):
+        SD = specific_dataset
+    else:
+        SD = None
     save_reward_hyperparameters(
         dataset_name, 
         batch_size, 
@@ -359,9 +366,9 @@ def train_reward(dataset_name: str, batch_size, num_steps, save_freq, lr, sigma,
            if step % save_freq == 0:
               checkpoint = copy.deepcopy(reward_net)
               save_model(checkpoint, reward_name, step)
-    save_to_finetuning(reward_net, dataset_name, specific_dataset)
+    save_to_finetuning(reward_net, dataset_name, SD)
     stats = get_pretrained_reward_stats(reward_name)
-    save_stats_to_finetuning(stats, dataset_name, specific_dataset)
+    save_stats_to_finetuning(stats, dataset_name, SD)
 
 class test_dataset(Dataset):
     def __init__(self, trajs, sigma, Reward_name, target_reward: Optional[float] = None, goal: Optional[np.array] = None):