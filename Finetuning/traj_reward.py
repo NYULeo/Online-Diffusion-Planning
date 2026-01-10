980f194a72136643fb6e64f6816300176c405650
NYULeo <leo@nyu.edu>
2026-01-10 00:12:58 -0800

    Update traj_reward.py: add debug print for avg log_prob

--- Finetuning/traj_reward.py
+++ Finetuning/traj_reward.py
@@ -74,6 +74,7 @@ class TotalReward(nn.Module):
             lp = self.kernels[i].log_prob(s_next, mu, log_std)
             total = total + lp 
         avg = total / len(self.kernels)
+        print(f"Avg: {avg}")
         x =  self.config.min_log_prob - avg
         c = F.softplus(x, beta = self.config.beta)
         return c
