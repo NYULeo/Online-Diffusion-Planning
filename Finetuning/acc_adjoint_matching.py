Update Python code: change checkpoint_steps to 140, base_seed to 40, fix gradient sign, update sigma to 7.0, enable score printing. Add kitchen planner checkpoints

Finetuning/acc_adjoint_matching.py
@@ -415,7 +415,7 @@ class Acc_AdjointMatchingFineTuner:
        
         if(reward_std == 0.0):
             reward_std = 1.0
-        a0 =  (-1 * (self.config.reward_scaling_factor/reward_std) * gradient).detach().unsqueeze(0).to(self.device) + (self.config.Entropy_Scaling_Factor * (-1) * EntGrad)
+        a0 =  (1 * (self.config.reward_scaling_factor/reward_std) * gradient).detach().unsqueeze(0).to(self.device) + (self.config.Entropy_Scaling_Factor * (-1) * EntGrad)
         #max_norm = 5.0
         #a0 =   a0 * torch.clamp(max_norm / torch.norm(a0), max=1.0)
         #print(f"a0: {a0.norm().item()}")