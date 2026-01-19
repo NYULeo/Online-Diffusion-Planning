1208c03495b897c67d6f1060adb58f1773aed697
NYULeo <leo@nyu.edu>
2026-01-18 19:06:16 -0800

    Update rollout and kernel training script.
    Adjust kernel training configuration and rollout behavior.

--- Pretrain/train_kernel_script2.py
+++ Pretrain/train_kernel_script2.py
@@ -6,14 +6,14 @@ os.chdir(project_root)
 from Transition_Kernel.Kernel_Backbone import train_kernel, test_kernel
 from utils import set_seed
 import pickle
+from Finetuning.utils import get_trajs
 
 
 if __name__ == '__main__':  # pragma: no cover
     set_seed(1)
     dataset = 'pointmaze'
     specific_dataset = 'large'
-    with open(f'./Pretrain/Rollouts/{dataset}/{specific_dataset}/Generated_trajs_Info.pkl', 'rb') as f:
-        trajs = pickle.load(f)
+    trajs = get_trajs(dataset, specific_dataset)
     #train_kernel(dataset_name = 'kitchen', batch_size = 256, lr = 1e-4, num_steps =  50000, ensemble_size=10, λ_reg=1e-3)
     #train_kernel(dataset_name = 'pointmaze', specific_dataset ='medium', batch_size = 256, lr = 3e-4, num_steps = 50000, ensemble_size=10, λ_reg=1e-3)
     #train_kernel(dataset_name = dataset, specific_dataset = specific_dataset, batch_size = 256, lr = 3e-4, num_steps = 30000, save_freq = 3000, ensemble_size = 10, hidden_layers = 5, λ_reg = 1e-3)
