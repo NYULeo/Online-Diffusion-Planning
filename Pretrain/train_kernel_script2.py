7aa3178636945a5ee180020ce37c7a0a4d840a8c
NYULeo <leo@nyu.edu>
2026-01-18 19:08:44 -0800

    Update kernel training script.
    Adjust kernel training parameters for current run.

--- Pretrain/train_kernel_script2.py
+++ Pretrain/train_kernel_script2.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':  # pragma: no cover
     set_seed(1)
     dataset = 'pointmaze'
     specific_dataset = 'large'
-    trajs = get_trajs(dataset, specific_dataset)
+    trajs = get_trajs(dataset, specific_dataset, 0)
     #train_kernel(dataset_name = 'kitchen', batch_size = 256, lr = 1e-4, num_steps =  50000, ensemble_size=10, λ_reg=1e-3)
     #train_kernel(dataset_name = 'pointmaze', specific_dataset ='medium', batch_size = 256, lr = 3e-4, num_steps = 50000, ensemble_size=10, λ_reg=1e-3)
     #train_kernel(dataset_name = dataset, specific_dataset = specific_dataset, batch_size = 256, lr = 3e-4, num_steps = 30000, save_freq = 3000, ensemble_size = 10, hidden_layers = 5, λ_reg = 1e-3)
