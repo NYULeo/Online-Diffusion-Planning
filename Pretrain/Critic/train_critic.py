e1fa681216ef8eff486f520fcb6c00a22131c5bf
NYULeo <leo@nyu.edu>
2026-01-18 17:51:34 -0800

    Move critic training entrypoint to script.
    Extracts the __main__ training routine into a standalone script.

--- Pretrain/Critic/train_critic.py
+++ Pretrain/Critic/train_critic.py
@@ -396,44 +396,6 @@ def test_critic(dataset_name: str, specific_dataset: str, checkpoint_step, sigma
     print(f"Average Loss: {avg_loss:.4f}")
 
 
-if __name__ == '__main__':  # pragma: no cover
-    set_seed(1)
-    env_name = 'pointmaze'
-    specific_env = 'large'
-    data = get_dataset(env_name, specific_env)
-    trajs = data.get_trajectories()
-    #trajs = get_trajs(env_name, specific_env, 0)
-    train_critic(dataset_name = env_name, 
-                 specific_dataset = specific_env, 
-                 sigma = 10.0, 
-                 batch_size = 512, 
-                 num_steps = 10000, 
-                 gamma = 1.0, 
-                 horizon = 32, 
-                 lr = 5e-5, 
-                 tau = 0.01,
-                 goal = None,
-                 target_reward = 1.0,
-                 trajs = trajs)
-    print('training complete')
-    
-    """
-    trajs = data.get_trajectories()
-    step = 2000
-    while(step <= 10000):
-        test_critic(dataset_name = env_name, 
-                    specific_dataset = specific_env, 
-                    checkpoint_step = step, 
-                    sigma = 10.0, 
-                    gamma = 1.0, 
-                    horizon = 32, 
-                    goal = None,
-                    target_reward = 50.0, 
-                    trajs = trajs)
-        step += 2000
-    print('testing complete')
-    """
-
 
 
 
