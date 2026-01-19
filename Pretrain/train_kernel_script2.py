beccfc54c60ca77e8a7224b9b01ebf7109a9ddcd
NYULeo <leo@nyu.edu>
2026-01-18 20:00:26 -0800

    Update kernel training and finetuning debug utilities.
    Refine kernel backbone logic, training script, and debugger settings.

--- Pretrain/train_kernel_script2.py
+++ Pretrain/train_kernel_script2.py
@@ -1,8 +1,8 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.chdir(project_root)
+#project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#os.chdir(project_root)
 from Transition_Kernel.Kernel_Backbone import train_kernel, test_kernel
 from utils import set_seed
 import pickle
