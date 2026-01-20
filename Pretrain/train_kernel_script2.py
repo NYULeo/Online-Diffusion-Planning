d74b2198f8abc8020301869a00e96dd943293596
NYULeo <leo@nyu.edu>
2026-01-20 00:06:36 -0800

    Update training script.
    Code-only update (Python files only).

--- Pretrain/train_kernel_script2.py
+++ Pretrain/train_kernel_script2.py
@@ -14,6 +14,9 @@ if __name__ == '__main__':  # pragma: no cover
     set_seed(1)
     dataset = 'pointmaze'
     specific_dataset = 'large'
+    path = "./Finetuning/Rollouts/pointmaze/large/Generated_trajs_Info.pkl"
+    with open(path, "rb") as f:
+          trajs = pickle.load(f)
     #train_kernel(dataset_name = 'kitchen', batch_size = 256, lr = 1e-4, num_steps =  50000, ensemble_size=10, λ_reg=1e-3)
     #train_kernel(dataset_name = 'pointmaze', specific_dataset ='medium', batch_size = 256, lr = 3e-4, num_steps = 50000, ensemble_size=10, λ_reg=1e-3)
     train_kernel(dataset_name = dataset, specific_dataset = specific_dataset, batch_size = 256, lr = 3e-4, num_steps = 30000, save_freq = 3000, ensemble_size = 10, hidden_layers = 5, λ_reg = 1e-3)
